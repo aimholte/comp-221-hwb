46d266a9e646b81d444d5d2b8d62a5cec1d95d6c
A.J <aimholte@macalester.edu>
2018-02-08 15:22:31 -0600

    new changes to kth

--- HWB/kth.py
+++ HWB/kth.py
@@ -2,6 +2,14 @@
 # given a list and an integer k,
 # return a list where the elements are L[k-1, 2k-1, 3k-1...n-1*k-1]
 
+# if the length of the list is less than or equal to the value of k
+    # return an empty list
+
+# else
+    # add the element at L[k-1] to a list (this will be returned)
+    # delete from the list in range of k
+    # call the function again on the new list with the same k
+
 # Keep track of what index we are at (set this equal to 1 at first)
 # If the index is greater than or equal to the length of list
     # Stop and return the new list
@@ -13,12 +21,10 @@
     # retun the list
 
 def kth(L,k):
-    kthList = []
-    if len(L) <= 0:
-        return 0
+    if len(L) <= k:
+        return []
     else:
-        element = L[len(L) * k - 1]
-        kthList.append(element)
+        element = [L[k -  1]]
         return kthList + kth(L[:-1], k)
     pass
 
